a659999e32e6e98e66393ae811f599f5e3d51509
jaweed <jaweed@LAPTOP-E0ODE6MM.mynetworksettings.com>
2023-04-06 16:41:30 -0400

    Initial Commit

---
+++ src/Test3/Test3.java
@@ -0,0 +1,14 @@
+package Test3;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Java is fan");
+		
+		System.out.println("Test3.....");
+		
+
+	}
+
+}
